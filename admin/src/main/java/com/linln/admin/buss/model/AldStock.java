b0cd0574c70431550ca6b01a3a82565af1d2b396
houkun <18514587664@163.com>
2020-01-13 17:48:06 +0800

    adda 供应商商品

---
+++ admin/src/main/java/com/linln/admin/buss/model/AldStock.java
@@ -0,0 +1,19 @@
+package com.linln.admin.buss.model;
+
+import lombok.Data;
+
+@Data
+public class AldStock {
+    
+    private String productID;
+    
+    private String size;
+    
+    private String stock;
+    
+    private String retailPrice;
+    
+    private String price;
+    
+    
+}
---
+++ admin/src/main/java/com/linln/admin/buss/model/AldStockResult.java
@@ -0,0 +1,12 @@
+package com.linln.admin.buss.model;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class AldStockResult {
+	
+	private List<AldStock> data;
+	
+}
